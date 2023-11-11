9b81ce7d024538257cd193a321a0015c8c5c5486
julia <ju.pennas@gmail.com>
2023-11-10 23:27:18 -0300

    user, email and password validation

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,4 +1,4 @@
-const { hash } = require("bcryptjs");
+const { hash, compare } = require("bcryptjs");
 const AppError = require("../utils/AppError");
 const sqliteConnection = require("../database/sqlite");
 
@@ -22,6 +22,52 @@ class UsersController {
         return response.status(201).json();
     }
     
+    async update(request, response) {
+        const { name, email, password, old_password } = request.body;
+        const { id } = request.params;
+
+        const database = await sqliteConnection();
+        const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+        if(!user) {
+            throw new AppError("Usuário não existe!");
+        }
+
+        const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+        if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+            throw new AppError("Esse e-mail já está sendo usado.");
+        }
+
+        user.name = name ?? user.name;
+        user.email = email ?? user.email;
+
+        if(password && !old_password) {
+            throw new AppError("Informe a senha antiga para definir a nova!");
+        }
+
+        if(password && old_password) {
+            const checkOldPassoword = await compare(old_password, user.password);
+
+            if(!checkOldPassoword) {
+                throw new AppError("A senha antiga não confere.");
+            }
+
+            user.password = await hash(password, 8);
+        }
+
+       await database.run(`
+        UPDATE users SET
+        name = ?,
+        email = ?,
+        password = ?, 
+        updated_at = ?
+        WHERE id = ?`,
+        [user.name, user.email, user.password, new Date(), id]
+        );
+
+        return response.json();
+    }
 };
 
 module.exports = UsersController;
--- src/server.js
+++ src/server.js
@@ -28,5 +28,5 @@ app.use((error, request, response, next) => {
     })
 })
 
-const PORT = 3333;
+const PORT = 3337;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
